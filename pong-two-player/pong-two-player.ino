a7989e5ef26911a1e9707fe393118c01a06f8cd0
Russell Polo <russellpolo@gmail.com>
2026-02-09 12:35:13 -0500

    added complex sound effects to pong- game

--- pong-two-player/pong-two-player.ino
+++ pong-two-player/pong-two-player.ino
@@ -52,12 +52,7 @@ int paddleRightY = 26;
 int lastPaddleLeftY = 26;   // ← ADD THESE
 int lastPaddleRightY = 26;  // ← FOR MASTER TRACKING
 
-/* ===== SOUND STATE ===== */
-bool soundActive = false;
-unsigned long soundEndTime = 0;
-int currentSoundFreq = 0;
-uint8_t triggered_sound=0;
-int shockwavesound=0; // not used but refrenced in sound helper
+
 
 
 /* ===== TIMING ===== */
@@ -68,6 +63,66 @@ uint8_t scoreLeft  = 0;
 uint8_t scoreRight = 0;
 
 
+
+
+/* sound triggers*/
+#define TRIGGER_SOUND_NONE 0x00
+#define TRIGGER_SOUND_BOUNCE 0x01
+#define TRIGGER_SOUND_MISS 0x02
+#define TRIGGER_SOUND_LOSE 0x03
+#define TRIGGER_SOUND_WIN 0x04
+
+/* ===== SOUND STATE ===== */
+bool soundActive = false;
+unsigned long soundEndTime = 0;
+int currentSoundFreq = 0;
+uint8_t triggered_sound=0;
+int shockwavesound=0; // not used but refrenced in sound helper
+int current_sound_index=0; // pointer to music notes array. 
+int currently_in_sound_gap=0; // used for sound effects that have multiple notes, to track the gap between them.
+
+#define G_freq   392
+#define Eb_freq 311
+#define c5_freq 523
+#define e5_freq 659
+#define g5_freq 784
+#define c6_freq 1047
+
+
+
+#define fire_freq 2200
+#define miss_freq 42
+#define note_gap  100 // gap between notes in ms, used for sound effects that have multiple notes.
+#define short_gap 20 // for quick notes like trimph 
+struct music_notes {
+  int tone;   // freqency in Hz
+  int duration; // duration in ms
+  int gap_after; // gap after the note in ms, used for multi-note sound effects.
+
+};
+
+
+
+const music_notes music[] PROGMEM = { 
+  {fire_freq, 40,0}, // short, sharp laser #FIRESOUND_INDEX=0
+  {miss_freq, 800,0}, // low buzz #MISSOUND_INDEX=1
+  {G_freq, 300,note_gap}, // Lose Sound , betoven. #LOSESOUND_INDEX=2
+  {G_freq, 300,note_gap}, 
+  {G_freq, 300,note_gap}, 
+  {Eb_freq, 900,0},
+  {c5_freq, 80,short_gap}, // triumph notes #WINSOUND_INDEX=6
+  {e5_freq, 80,short_gap},
+  {g5_freq, 80,short_gap},
+  {c6_freq, 80,short_gap},
+  {g5_freq, 400,0}, // hold last note
+  {0, 0,0} // end marker
+};
+// these are manual pointers into the music array.
+#define FIRESOUND_INDEX 0
+#define MISSOUND_INDEX 1
+#define LOSESOUND_INDEX 2
+#define WINSOUND_INDEX 6
+
 /* ===== PACKETS ===== */
 #define PKT_DISCOVERY 0x01
 #define PKT_INPUT     0x02
@@ -76,11 +131,8 @@ uint8_t scoreRight = 0;
 #define PKT_FORCE_READY 0x05
 #define PKT_YOUARECLIENT 0x06
 
-/* sound triggers*/
-#define TRIGGER_SOUND_NONE 0x00
-#define TRIGGER_SOUND_BOUNCE 0x01
-#define TRIGGER_SOUND_MISS 0x02
 
+/* ===== PACKET STRUCTS ===== */
 
 struct DiscoveryPacket {
   uint8_t type;
@@ -116,6 +168,7 @@ struct ForceReadyPacket {
   uint8_t type;   // PKT_FORCE_READY
   uint8_t scoreLeft;
   uint8_t scoreRight;
+  uint8_t sound_trigger;
 };
 
 struct YouAreClient {
@@ -251,6 +304,7 @@ if (type == PKT_READY) {
     gameState = STATE_READY;
     scoreLeft  = pkt.scoreLeft;
     scoreRight = pkt.scoreRight;
+    process_sound_trigger(pkt.sound_trigger);
     localReady = remoteReady = false;
     return;
   }
@@ -339,6 +393,16 @@ void resetBall(bool towardLeft) {
     localReady  = false;
     remoteReady = false;
 
+    // who won?
+    if (scoreLeft > 10) {
+      Serial.println("LEFT PLAYER WINS!");
+      winSound();
+    } else {
+      Serial.println("RIGHT PLAYER WINS!");
+      looseSound();
+
+    }
+
     sendForceReady();
     return;   // DO NOT spawn a new ball
   }
@@ -376,31 +440,49 @@ void sendForceReady() {
   pkt.type = PKT_FORCE_READY;
   pkt.scoreLeft = scoreLeft;
   pkt.scoreRight = scoreRight;
+  pkt.sound_trigger=triggered_sound;
   esp_now_send(partnerMac, (uint8_t*)&pkt, sizeof(pkt));
 }
 
-/* sound functions */
+/* ===================================================== */
+/* ================== SOUND HELPERS =================== */
+/* ===================================================== */ 
 
-void startSound(int freq, unsigned long durationMs) {
-  if (soundActive) return;   // don't interrupt existing sound
 
+void startSound(int index) {
+  //if (soundActive) return;   // don't interrupt existing sound
+
+  current_sound_index = index;
+  int freq = music[index].tone;
+  int durationMs = music[index].duration;
   currentSoundFreq = freq;
   soundEndTime = millis() + durationMs;
   soundActive = true;
-
+  currently_in_sound_gap=0; // we are playing a note, not a gap. 
+  Serial.print("Starting sound index "); Serial.print(index);
+  Serial.print(" freq "); Serial.print(freq);
   tone(BUZZER_PIN, freq);
 }
 
 void fireSound() {
-  startSound(2200, 40);   // short, sharp laser
+  startSound(FIRESOUND_INDEX);   // short, sharp laser
   triggered_sound=TRIGGER_SOUND_BOUNCE;
 }
 
 void missSound() {
-  startSound(42, 800);   // low buzz
+  startSound(MISSOUND_INDEX);   // low buzz
   triggered_sound=TRIGGER_SOUND_MISS;
 }
 
+void looseSound() {
+  startSound(LOSESOUND_INDEX);   // betoven notes
+  triggered_sound=TRIGGER_SOUND_WIN;
+}
+
+void winSound() {
+  startSound(WINSOUND_INDEX);   // trimph notes
+  triggered_sound=TRIGGER_SOUND_LOSE; // the other side plays the lose sound, so trigger that one
+} 
 
 void updateSound() {
   if (shockwavesound>0) {
@@ -412,9 +494,25 @@ void updateSound() {
 
   if (soundActive && millis() >= soundEndTime) {
     noTone(BUZZER_PIN);
+    // check for multi-note sound effects
+    if (music[current_sound_index].gap_after == 0) {
     soundActive = false;
     shockwavesound=0;
-  }
+    Serial.println("Sound ended");
+    } else {
+      // did we play the gap between notes already? if not, play it now.
+      if ((currently_in_sound_gap==0) && (music[current_sound_index].gap_after > 0)) {
+        Serial.println("starting gap");
+        // we just played a note, now play the gap before the next note.
+        noTone(BUZZER_PIN); // silence for gaps
+        currently_in_sound_gap=1; // we are in a gap between notes. 
+        soundEndTime = millis() + music[current_sound_index].gap_after; // set end time for the gap
+      } else {
+        Serial.println("gap ended, play next");
+        startSound(current_sound_index+1); // play the next note in the effect
+      }
+    }
+}
 }
 
 void process_sound_trigger(uint8_t trigger){
@@ -423,6 +521,19 @@ void process_sound_trigger(uint8_t trigger){
    fireSound();
   return;
  }
+if (TRIGGER_SOUND_MISS == trigger){
+   missSound();
+  return;
+ }
+ if (TRIGGER_SOUND_LOSE == trigger){
+   looseSound();
+  return;
+ }
+ if (TRIGGER_SOUND_WIN == trigger){
+   winSound();
+  return;
+ }
+
 
 }
 
